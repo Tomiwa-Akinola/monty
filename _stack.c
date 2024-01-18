46977ece9a194201189c70ddb84fa7c6ea4e673d
tomiwa <codertomiwa@gmail.com>
2024-01-18 10:00:49 -0800

    _stack.c

---
+++ _stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _stack - prints the top
+ * @head: stack head
+ * @count: line_number
+*/
+void _stack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 0;
+}
