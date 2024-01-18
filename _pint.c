f2add93706528b2ec760e76ee8d3036e6a216864
tomiwa <codertomiwa@gmail.com>
2024-01-18 05:29:52 -0800

    _pint.c

---
+++ _pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * _pint - prints the top of stack
+ * @head: stack head
+ * @count: line number
+*/
+void _pint(stack_t **head, unsigned int count)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+
