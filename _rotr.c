7e817f066e28f7fd6783f01b72adaf525f009b5d
tomiwa <codertomiwa@gmail.com>
2024-01-18 09:58:02 -0800

    _rotr.c

---
+++ _rotr.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+  *_rotr - rotates stack to bottom
+  *@head: stack head
+  *@count: line_number
+  *Return: no return
+ */
+void _rotr(stack_t **head, __attribute__((unused)) unsigned int count)
+{
+	stack_t *cpy;
+
+	cpy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (cpy->next)
+	{
+		cpy = cpy->next;
+	}
+	cpy->next = *head;
+	cpy->prev->next = NULL;
+	cpy->prev = NULL;
+	(*head)->prev = cpy;
+	(*head) = cpy;
+}
